a340018b3af6861a9560a7713598054b796a050f
Javohir <your_email>
2025-07-26 15:08:48 +0500

    asd

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'olcha',
     'users',
     'rest_framework',
